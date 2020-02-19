cf644774f20a539c5fbe8f57ffa6068581ee552a
Nimrod Oron <nimrod.oron@sap.com>
2020-02-19 17:10:33 +0200

    fixes

--- charts/seed-operatingsystemconfig/downloader/templates/scripts/_init-containerd.sh
+++
@@ -1,9 +0,0 @@
-{{- define "init-containerd-config-script" -}}
-#!/bin/bash
-
-FILE=/etc/containerd/config.toml
-if [ ! -f "$FILE" ]; then
-  mkdir -p /etc/containerd
-  containerd config default > "$FILE"
-fi
-{{- end -}}
